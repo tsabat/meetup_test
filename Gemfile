03ba153db342e4c572e0e34aaf5fef9b3488c322
Timothy Sabat <timothy.sabat@gmail.com>
2014-09-15 17:41:28 -0700

    some syntax stuff

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
-gem 'jazz_hands', github: 'nixme/jazz_hands', branch: 'bring-your-own-debugger'
+#gem 'jazz_hands', github: 'nixme/jazz_hands', branch: 'bring-your-own-debugger'
 gem 'pry-byebug'
 
 gem 'httparty'
--- app/services/meetup_api.rb
+++ app/services/meetup_api.rb
@@ -1,7 +1,7 @@
 class MeetupApi
   include HTTParty
   base_uri 'http://api.meetup.com/'
-  API_KEY = #API KEY
+  API_KEY = ''
 
   attr_accessor :options
 
@@ -9,18 +9,18 @@ class MeetupApi
     @options = { query: {key: API_KEY} }
   end
 
-  def events(group_id:, status: 'past', desc: true, time: '1316121739488,1410816101633')
+  def events(group_id, status: 'past', desc: true, time: '1316121739488,1410816101633')
     @options[:query].merge!(group_id: group_id, status: status, time: time, desc: desc)
 
     self.class.get("/2/events", @options)
   end
 
-  def members(group_id:)
+  def members(group_id)
     @options[:query].merge!(group_id: group_id)
     self.class.get("/2/members", @options)
   end
 
-  def attendance(urlname:, event_id:)
+  def attendance(urlname, event_id)
     self.class.get("/#{urlname}/events/#{event_id}/attendance", @options)
   end
 end
